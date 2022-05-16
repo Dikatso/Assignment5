c0ff9ff1d6234dd59a4422f1673f9bedb80844b0
Dikatso Moshweunyane <dikatsogaleboe@gmail.com>
2022-05-16 04:43:50 +0200

    Added a decaying learning rate

--- Scenario1.py
+++ Scenario1.py
@@ -3,66 +3,87 @@
 # CSC3002 ASSIGN5 Scenario1
 
 import numpy as np
+import pandas as pd
 import random
 from matplotlib import pyplot, colors
 from FourRooms import FourRooms
 
-
+def deploy_agent(init_state,q_table):
+    print("Start: ", init_state)
+    state = init_state
+    steps = 0
+    x = 0
+    while x < 10:
+        step +=1
+        action = np.argmax(q_table[state,:])
+        print(action)
+        state = action
+        x += 0
+        
+# def shortest_path(begin,end):
+#     path = [begin]
+#     next_node = np.argmax(q_table[begin,])
+#     path.append(next_node)
+#     while next_node != end:
+#         next_node = np.argmax(q_table[next_node,])
+#         path.append(next_node)
+#     return path
 
 
 def main():
     
-    # q_table = np.array(
-    #         [
-    #             # 0   1   2   3   4   5   6   7   8   9  10  11  12
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #         ], dtype=np.float32
-    #     )
+    q_table = np.array(
+            [
+                # 0   1   2   3   4   5   6   7   8   9  10  11  12
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100], 
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100]
+            ], dtype=np.float32
+        )
     
-    # r_table = np.array(
-    #         [
-    #             # 0   1   2   3   4   5   6   7   8   9  10  11  12
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #         ], dtype=np.float32
-    #     )
+    r_table = np.array(
+            [
+                # 0   1   2   3   4   5   6   7   8   9  10  11  12
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ], dtype=np.float32
+        )
     
     
-    EPOCH = 100
+    EPOCH = 5000
     LEARNING_RATE = 0.7
     DISCOUNT_FACTOR = 0.9
-    EPLISON =1
+    EPLISON = 0.9
     MAX_EPSILON =1.0
     MIN_EPSILON = 0.01
-    DECAY_RATE = 0.01
+    DECAY_RATE = 0.001
     
     rewards_track = 0
     # Create FourRooms Object
     fourRoomsObj = FourRooms('simple')
-    q_table = np.zeros((13, 13))
+    global_start = fourRoomsObj.getPosition()
+    # q_table = np.zeros((13, 13))
 
     print(q_table)
     
@@ -75,35 +96,81 @@ def main():
         while notDone:
             if random.uniform(0, 1) > EPLISON:
                 # return np.argmax(q_values[current_row_index, current_column_index])
-                nextStep = np.argmax(q_table[currentPosition[0],currentPosition[1]]) 
+                nextStep = np.argmax(q_table[currentPosition,:]) 
             else:
                 nextStep = random.randint(0, 3)
         
             gridType, newPos, packagesRemaining, isTerminal = fourRoomsObj.takeAction(nextStep)
             
-            # Update Q-table for Q(s,a)
-            if q_table[newPos[0], newPos[1]] == 0:
-                reward = -1
-                
-            else:
-                reward = q_table[newPos[0], newPos[1]]
+            reward = r_table[newPos[1], newPos[0]]
+            q_table[newPos[1], newPos[0]] = q_table[newPos[1], newPos[0]] * (1 - LEARNING_RATE) + LEARNING_RATE * (reward + DISCOUNT_FACTOR * np.max(q_table[newPos[1], newPos[0]]))
             
-            q_table[newPos[0], newPos[1]] = q_table[newPos[0], newPos[1]] * (1 - LEARNING_RATE) + LEARNING_RATE * (reward + DISCOUNT_FACTOR * np.max(q_table[newPos[0], newPos[1]]))
+            r_table[newPos[1], newPos[0]] = -1
+            
+            # # Update Q-table for Q(s,a)
+            # if q_table[newPos[0], newPos[1]] == 0:
+            #     reward = -1
+                
+            # else:
+            #     reward = q_table[newPos[0], newPos[1]]
+            
+            # q_table[newPos[0], newPos[1]] = q_table[newPos[0], newPos[1]] * (1 - LEARNING_RATE) + LEARNING_RATE * (reward + DISCOUNT_FACTOR * np.max(q_table[newPos[0], newPos[1]]))
             if isTerminal:
                 notDone = False
+                r_table[newPos[1], newPos[0]] = 100
                 #q_table[newPos[0], newPos[1]] = 100
-                print("yeee")
+                # print("yeee")
                 break
-            
+        EPLISON = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * np.exp(-DECAY_RATE * epoch)   
         
-        normalize_q=q_table/max(q_table[q_table.nonzero()])*100
-        normalize_q.astype(int)
+        # normalize_q=q_table/max(q_table[q_table.nonzero()])*100
+        # normalize_q.astype(int)
+        print("Number ", epoch)
         
-        print(q_table)
+    print(pd.DataFrame(q_table))
+    print(pd.DataFrame(r_table))
         # print("normalized")
         # print(normalize_q)
         
-    fourRoomsObj.showPath(-1)    
+    fourRoomsObj.showPath(-1) 
+    print("--------------------------------Out------------------------------")
+    # print(shortest_path(0, 10))
+    begin = 0
+    end = 3
+    
+    
+    path = [begin]
+    next_node = np.argmax(q_table[begin,])
+    path.append(next_node)
+    while next_node != end:
+        print("working")
+        next_node = np.argmax(q_table[next_node,])
+        path.append(next_node)
+    print(path)
+    # deploy_agent(global_start,q_table)
+    # print("Start: ", global_start)
+    # state = global_start
+    # steps = 0
+    # x = 0
+    # print(q_table[global_start,:])
+    # while x < 10:
+    #     steps +=1
+        # action = np.argmax(q_table[global_start,:])
+        # print(action)
+        # state = action
+    #     x += 1
+    
+    # def getShortest(initialState, fourRoomsObj, aTypes, gTypes):
+    # done = False
+    # while not done:
+    #     action = epsilonFuction(initialState, epsilon)
+    #     gridType, newPos, packagesRemaining, isTerminal = fourRoomsObj.takeAction(action)
+    #     print("Agent took {0} action and moved to {1} of type {2}".format (aTypes[action], newPos, gTypes[gridType]))
+    #     if isTerminal:
+    #         done = True
+    #     initialState = newPos
+    # fourRoomsObj.showPath(-1)
+    
         
     # Create FourRooms Object
     # fourRoomsObj = FourRooms('simple')
